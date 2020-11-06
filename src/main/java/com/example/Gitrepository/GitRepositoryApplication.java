f22c735a34594fb27a5054d2fc5659cf0b192edd
Amrish <amrishvr17@gmail.com>
2020-11-06 23:41:40 +0530

    feature branch

--- src/main/java/com/example/Gitrepository/GitRepositoryApplication.java
+++ src/main/java/com/example/Gitrepository/GitRepositoryApplication.java
@@ -3,7 +3,7 @@ package com.example.Gitrepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Testing git again
+// feature branch
 @SpringBootApplication
 public class GitRepositoryApplication {
 
