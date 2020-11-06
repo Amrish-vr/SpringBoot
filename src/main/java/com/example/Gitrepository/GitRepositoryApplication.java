cff94404dac9e073ca2ba08d11c3b7a854218b0c
Amrish <amrishvr17@gmail.com>
2020-11-07 00:14:55 +0530

    final commit

--- src/main/java/com/example/Gitrepository/GitRepositoryApplication.java
+++ src/main/java/com/example/Gitrepository/GitRepositoryApplication.java
@@ -3,7 +3,6 @@ package com.example.Gitrepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// default branch
 @SpringBootApplication
 public class GitRepositoryApplication {
 
