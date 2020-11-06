c07bf77327c873feefd6fd0e140995d2a61851c1
Amrish <amrishvr17@gmail.com>
2020-11-06 23:18:47 +0530

    Second commit

--- src/main/java/com/example/Gitrepository/GitRepositoryApplication.java
+++ src/main/java/com/example/Gitrepository/GitRepositoryApplication.java
@@ -3,6 +3,8 @@ package com.example.Gitrepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Testing git
 @SpringBootApplication
 public class GitRepositoryApplication {
 
