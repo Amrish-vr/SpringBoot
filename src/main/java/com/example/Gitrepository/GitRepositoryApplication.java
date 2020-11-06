3f40f974999999971a2f172f75a26e95c9b1fa6e
Amrish <amrishvr17@gmail.com>
2020-11-06 23:17:26 +0530

    First commit

---
+++ src/main/java/com/example/Gitrepository/GitRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gitrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRepositoryApplication.class, args);
+	}
+
+}
